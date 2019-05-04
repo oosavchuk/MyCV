5c78fd63097af2c2f9662e9ed2e079f1fca1e693
Oleksandr <alexander.osavchuk@gmail.com>
2019-05-04 10:29:38 +0300

    Initial Commit

---
+++ MyCV/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  MyCV
+//
+//  Created by Oleksandr  on 5/4/19.
+//  Copyright © 2019 Oleksandr . All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ MyCV/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  MyCV
+//
+//  Created by Oleksandr  on 5/4/19.
+//  Copyright © 2019 Oleksandr . All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
